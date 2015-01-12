101750abb9856220632b7ddc57fb6d8f515a27bb
MaasMeena <meena_vas@yahoo.com>
2015-01-11 19:27:29 -0500

    MyProgram
    My Program Entity

---
+++ MyProgram.java
@@ -0,0 +1,18 @@
+Class MyProgram{
+  private String programName;
+  private int programId;
+  
+  public String getProgramName(){
+    return programName;
+  }
+  public void setProgramName(String programName){
+    this.programName = programName
+  }
+  public int getProgramId(){
+    return programId;
+  }
+  public void setProgramId(int programId){
+    this.programId = programId;
+  }
+
+}
